d307707138d45a113459888f4ed440f4ef70198a
Yuriko Hoshizawa <hoshizawa.yuriko@gmail.com>
2025-03-19 09:10:08 +0100

    Add spring boot initializer project files

---
+++ src/main/java/com/example/alv/AlvApplication.java
@@ -0,0 +1,13 @@
+package com.example.alv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlvApplication.class, args);
+	}
+
+}
